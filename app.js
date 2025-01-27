1c1f680e1587db00fac22563f192bc1f01e8d746
Amy <amy_hanwell@hotmail.com>
2025-01-27 18:48:51 +0000

    GET api/articles/:article_id responds with article of passed ID

--- app.js
+++ app.js
@@ -1,40 +1,58 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("./controller/controllers");
+const { getTopics, getArticleById } = require("./controller/controllers");
 const endpoints = require("./endpoints.json");
 
 app.use(express.json());
 
-// GET requests 
+// GET requests
 
 app.get("/api", (req, res) => {
-  res.status(200).send( { endpoints } );
+  res.status(200).send({ endpoints });
 });
 
-app.get("/api/topics", getTopics)
+app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
 
-// error handling middleware
+// ** error handling middleware ** //
 
-app.use((err, req, res, next) => {
+// non existent endpoint catchall
+app.all("*", (err, req, res, next) => {
   if (err) {
-    res.status(400).send({ error: "Bad Request" });
+    res.status(404).send({ msg: "Not Found" });
   } else {
-    next(err); // pass it on to the next handler
+    next(err); 
   }
 });
 
 app.use((err, req, res, next) => {
-    if (err) {
-      res.status(404).send({ error: "Not Found" });
+    if (err.status) {
+      res.status(error.status).send({ msg: err.msg });
     } else {
-      next(err); // pass it on to the next handler
+      next(err); 
+    }
+  });
+
+  app.use((err, req, res, next) => {
+    if (err.code === "22P02") {
+      res.status(400).send({ msg: "Bad Request" });
+    } else {
+      next(err); 
     }
   });
 
 app.use((err, req, res, next) => {
+  if (err) {
+    res.status(404).send({ msg: "Not Found" });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
   if (err);
-  res.status(500).send({ error: "Internal Server Error" });
+  res.status(500).send({ msg: "Internal Server Error" });
 });
 
 module.exports = app;
